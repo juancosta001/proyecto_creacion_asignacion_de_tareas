a992e47730a4b2d943288ad81c9b2fbf01065940
juan <juanacostapereira9@gmail.com>
2025-04-04 20:19:50 -0300

    Se modifica la estructura general de la BD

---
+++ database/migrations/2025_04_04_230507_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->date('start_date');
+            $table->date('estimated_end_date')->nullable();
+            // Estado del proyecto: in_progress o completed.
+            $table->enum('status', ['in_progress', 'completed'])->default('in_progress');
+            // Relación con la tabla clients.
+            $table->foreignId('client_id')->constrained('clients')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
